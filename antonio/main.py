4193ab0705308573c7ee1cca525d2fab3f3f81e9
Antonio Sollazzo <a.sollazzo@studenti.poliba.it>
2026-06-12 16:12:01 +0200

    v0.1

---
+++ antonio/main.py
@@ -0,0 +1,67 @@
+import modulo_Utente
+
+utenti: list[modulo_Utente.Cliente] = []
+
+def crea_utente():
+#funzione di registrazione    
+    print("Inserisci nome e cognome")
+    nome_cognome = input()
+    
+    print("Inserisci indirizzo mail")
+    email = input()
+    
+    print("Inserisci password")
+    password = input()
+    
+    utente_temp = modulo_Utente.Cliente(nome_cognome, email, password)
+    flag_gia_esistente = False
+    
+    for utente in utenti: #grazie a questo metodo evito di avere la stessa mail inserita piu volte
+        if email == utente.email:
+            break
+    if not flag_gia_esistente:  #grazie al flag riesco a capire se è gia presente
+            utenti.append(utente_temp)
+            print("Cliente aggiunto con successo")
+            
+#grazie a questo decoratore posso controllare preventivamente che l'utente esista davvero
+def check_utenti(func):
+    def wrapper():
+        print("Inserisci indirizzo email: ")
+        email = input()
+        flag_trovato = False
+        for utente in utenti:
+            if email == utente.email:
+                func(email) #eseguo la funzione solo SE la mail è presente
+                break
+        if not flag_trovato:
+            print("Utente non trovato")
+        return wrapper
+
+@check_utenti
+def login(email):
+    
+    print("Inserisci password: ")
+    password = input()
+    for utente in utenti:
+        if email == utente.email and password == utente.password:   #se entrambi i campi sono giusti, si procede col login
+            return utente
+        elif password != utente.password:
+            print("Password non corretta") 
+
+while True:
+    print("1. Accedi")
+    print("2. Registrati")
+    
+    selettore = int(input())
+    
+    match selettore:
+        case 1:
+            utente = crea_utente()
+        case 2:
+            pass
+        case 3:
+            print("Arrivederci")
+            break
+        case _:
+            print("Selezione errata")
+        
--- antonio/modulo_Utente.py
+++ antonio/modulo_Utente.py
@@ -1,13 +1,12 @@
 class Cliente:
     
-    n_clienti = 0
+    n_clienti = 0   #attriburo di classe usato per fare l'auto increment
     
     def __init__(self, nome_cognome, email, password):
         self.nome_cognome = nome_cognome
         self.email = email
         self.password = password
         self.id_cliente = Cliente.conta_clienti() + 1 #in questo modo ho un intero incrementale, grazie al metodo statico che prima mi conta i clienti e poi assegna un +1 a tale valore
-        self.wallet = 0 #il credito è zero inizialmente, è da prevedere un metodo per ricaricare il saldo
     
     @classmethod
     def conta_clienti(cls):
